455af2dd999250892bab636a1c520ce7f671e262
Daniel Park <joymountainpark@gmail.com>
2015-03-22 23:43:10 +0900

    Create new recipe controller and action

--- app/controllers/myrecipes_controller.rb
+++ app/controllers/myrecipes_controller.rb
@@ -4,4 +4,30 @@ class MyrecipesController < ApplicationController
 		@myrecipes = Myrecipe.all
 	end
 
+	def show
+		@myrecipe = Myrecipe.find(params[:id])
+	end
+
+	def new
+		@myrecipe = Myrecipe.new
+	end
+
+	def create
+		@myrecipe = Myrecipe.new(myrecipe_params)
+		@myrecipe.chef = Chef.find(2)
+
+		if @myrecipe.save
+			flash[:success] = "Your recipe was created successfully!"
+			redirect_to myrecipes_path
+		else
+			render :new
+		end
+	end
+
+	private 
+
+		def myrecipe_params
+			params.require(:myrecipe).permit(:name, :summary, :description)
+		end
+
 end
